e8c94eaae4afd84e60c2d71ecf29c30d60d44514
Darren Yeoh <cheanglengg@gmail.com>
2021-08-15 21:32:58 +1000

    feat(portfolio): added prismic headless cms
    affects: dyeoh

--- packages/portfolio/gatsby-config.js
+++ packages/portfolio/gatsby-config.js
@@ -1,7 +1,21 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "Darren Yeoh",
   },
-  plugins: ["@chakra-ui/gatsby-plugin"],
+  plugins: [
+    "@chakra-ui/gatsby-plugin",
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+      },
+    },
+  ],
 };
